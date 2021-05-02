e0397bfdca9cf50b1e968cb03e7ca317001655a2
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-05-02 15:17:27 +0530

    removed testing schema

--- Intro to GraphQL/src/server.js
+++ Intro to GraphQL/src/server.js
@@ -11,21 +11,9 @@ const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
   const rootSchema = `
-    type Cat{
-      name: String
-      age: Int
-      owner:Owner!
-    }
-    type Owner{
-      name: String
-      cat: Cat!
-    }
-    type Query2{
-      cat(name:String): Cat!
-      owner(name:String): Owner!
-    }
+  
     schema {
-      query: Query2
+      query: Query
       mutation: Mutation
     }
   `
@@ -33,36 +21,7 @@ export const start = async () => {
 
   const server = new ApolloServer({
     typeDefs: [rootSchema, ...schemaTypes],
-    // resolvers: merge({}, product, coupon, user),
-    resolvers: {
-      Query2: {
-        cat(_,args){
-          return {}
-        },
-        owner(_,args){
-          return {}
-        }
-      },
-      Cat:{
-        name(){
-          return 'Garfield'
-        },
-        age(){
-          return 5
-        },
-        owner(){
-          return{}
-        }
-      },
-      Owner:{
-        name(){
-          return 'Joe'
-        },
-        cat(){
-          return{}
-        }
-      }
-    },
+    resolvers: merge({}, product, coupon, user),
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
