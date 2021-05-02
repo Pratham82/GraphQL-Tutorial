6976b59dc2fb57f0773c583d7cc13eef62e2f5d7
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-05-02 15:18:22 +0530

    Added product resolvers

--- Intro to GraphQL/src/types/product/product.resolvers.js
+++ Intro to GraphQL/src/types/product/product.resolvers.js
@@ -2,6 +2,7 @@ import { Product } from './product.model'
 import { User, roles } from '../user/user.model'
 import { AuthenticationError } from 'apollo-server'
 import mongoose from 'mongoose'
+import userResolvers from '../user/user.resolvers'
 
 const productsTypeMatcher = {
   GAMING_PC: 'GamingPc',
@@ -9,10 +10,35 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const products = () => {
+  return Product.find({}).exec()
+}
+const product  = (_,args) => {
+  return Product.findById({_id:args.id}).exec()
+}
+const newProduct = (_,args,ctx) =>{
+  return Product.create({...args.input, createdBy: ctx.user._id})
+}
+const updateProduct = (_, args) =>{
+  return Product.findByIdAndUpdate(args.id , args.input, {new:true}).lean().exec()
+}
+const removeProduct = (_,args) => {
+  return Product.findByIdAndDelete(args.id).lean().exec()
+}
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    products,
+    product,
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy(product) {
+      return User.findById(product.createdBy)
+    }
   }
 }
