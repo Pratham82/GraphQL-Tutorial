aef74e393d418dd9515c080fab811c6a3a46efaa
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-05-02 16:51:20 +0530

    Added interface example

--- Intro to GraphQL/src/server.js
+++ Intro to GraphQL/src/server.js
@@ -10,18 +10,51 @@ import user from './types/user/user.resolvers'
 const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
-  const rootSchema = `
+  const animalSchema = `
+    interface Animal {
+      species: String!
+      location: String!
+    }
   
+    type Tiger implements Animal {
+       species: String!
+        location: String!
+        stripesCount: Int
+      }
+    type Lion implements Animal {
+      species: String!
+      location: String!
+      mainColor: String
+     }
+     type Query{
+       animals: [Animal]!
+     }
+  `
+  const rootSchema = ` 
     schema {
       query: Query
       mutation: Mutation
-    }
-  `
+    }`
   const schemaTypes = await Promise.all(types.map(loadTypeSchema))
 
   const server = new ApolloServer({
-    typeDefs: [rootSchema, ...schemaTypes],
-    resolvers: merge({}, product, coupon, user),
+    typeDefs: [animalSchema],
+    // typeDefs: [rootSchema, ...schemaTypes],
+    // resolvers: merge({}, product, coupon, user),
+    resolvers: {
+      Query:{
+        animals() {
+          return[
+            {species: 'Tiger', location: 'BL', stripesCount: 232},{species: 'Lion', location:'MH',mainColor:'yellow'}
+          ] 
+        }
+      },
+      Animal:{
+        __resolveType(animal){
+           return animal.species
+        }
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
