ae3c7cc7afb127c57f03b59e81ec5f7a54349c21
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-05-02 14:28:52 +0530

    Learning resolvers

--- Intro to GraphQL/src/server.js
+++ Intro to GraphQL/src/server.js
@@ -11,8 +11,21 @@ const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
   const rootSchema = `
+    type Cat{
+      name: String
+      age: Int
+      owner:Owner!
+    }
+    type Owner{
+      name: String
+      cat: Cat!
+    }
+    type Query2{
+      cat(name:String): Cat!
+      owner(name:String): Owner!
+    }
     schema {
-      query: Query
+      query: Query2
       mutation: Mutation
     }
   `
@@ -20,7 +33,36 @@ export const start = async () => {
 
   const server = new ApolloServer({
     typeDefs: [rootSchema, ...schemaTypes],
-    resolvers: merge({}, product, coupon, user),
+    // resolvers: merge({}, product, coupon, user),
+    resolvers: {
+      Query2: {
+        cat(_,args){
+          return {}
+        },
+        owner(_,args){
+          return {}
+        }
+      },
+      Cat:{
+        name(){
+          return 'Garfield'
+        },
+        age(){
+          return 5
+        },
+        owner(){
+          return{}
+        }
+      },
+      Owner:{
+        name(){
+          return 'Joe'
+        },
+        cat(){
+          return{}
+        }
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
